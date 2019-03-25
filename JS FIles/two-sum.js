3b4fc4cb933e4f5935369993545becc6b650f3b1
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-03-24 20:58:41 -0700

    added previously solved problems

---
+++ JS FIles/two-sum.js
@@ -0,0 +1,20 @@
+var twoSum = function (nums, target) {
+    // let pointerA = 0;
+    // let pointerB = nums.length - 1;
+    // let sum = nums[pointerA] + nums[pointerB];
+    // while(!(sum===target)){
+    //     if(sum>target) {
+    //         pointerB--
+    //     }else {
+    //         pointerA++
+    //     }
+    //     sum = nums[pointerA] + nums[pointerB];
+    var sum = 0;
+    for (let x = 0; x < nums.length; x++) {
+        for (let y = 0; y < nums.length; y++) {
+            if (x === y) continue;
+            sum = nums[x] + nums[y];
+            if (sum === target) return [x, y]
+        }
+    }
+}
