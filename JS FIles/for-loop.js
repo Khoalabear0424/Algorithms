2f28da508e06c1bc5c36b8cdc5afeca2fa0e1644
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-03-28 18:32:08 -0700

    tried to make a matrix

--- JS FIles/for-loop.js
+++ JS FIles/for-loop.js
@@ -1,4 +1,9 @@
 
+var matrix = [];
 for (var i = 0; i < 10; i++) {
-    console.log(i);
+    for (var j = 0; j < 10; j++) {
+        matrix[i, j] = Math.floor(Math.random() * 10);
+    }
 }
+
+console.log(matrix);
