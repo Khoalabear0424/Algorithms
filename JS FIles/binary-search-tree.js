e5ae8192b1cbb52506f4907a465d1def5756f97f
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-04-03 09:51:16 -0700

    added BFS, DFSPreOrder, DFSPostOrder

--- JS FIles/binary-search-tree.js
+++ JS FIles/binary-search-tree.js
@@ -78,6 +78,17 @@ class BST {
         traverse(current);
         return data;
     }
+    DFSPostOrder(){
+        var data = [];
+        var current = this.root;
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+            data.push(node.value);
+        }
+        traverse(current);
+        return data;
+    }
 }
 
 var tree = new BST();
