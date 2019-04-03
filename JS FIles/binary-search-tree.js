47f77672a19e3094a5733405aa61740deb6196fd
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-04-03 09:36:38 -0700

    Create binary-search-tree.js

---
+++ JS FIles/binary-search-tree.js
@@ -0,0 +1,79 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BST {
+    constructor(){
+        this.root = null;
+    }
+    search(value){
+        if(this.root === null) return false;
+        var current = this.root;
+        var found = false;
+        while(!found && current){
+            if(value < current.value){
+                current = current.left;
+            }else if(value > current.value){
+                current = current.right;
+            }else {
+                found = true;
+            }
+        }
+        return current;
+    }
+
+    insert(value){
+        if(value === this.root) return undefined;
+        var newNode = new Node(value)
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        }else {
+            var current = this.root;
+            while(true){
+                if(value < current.value){
+                    if(current.left === null){
+                        current.left = newNode;
+                        return;
+                    }else{
+                        current = current.left;
+                    }
+                }else if(value > current.value){
+                    if(current.right === null){
+                        current.right = newNode;
+                        return;
+                    }else{
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
+    BFS(){
+        var data = [],
+           queue = [],
+            node = this.root
+
+        queue.push(node);
+        while(queue.length){
+            node = queue.shift();
+            data.push(node.value);
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+        return data;
+    }
+}
+
+var tree = new BST();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
