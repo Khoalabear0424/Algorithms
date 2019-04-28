4939bed5aa97b62aa6d40bbd2edfc5f630d68a30
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-04-28 12:33:21 -0700

    solve isunique

---
+++ Cracking-The-Coding-Interview/arrays-strings/is-unique.js
@@ -0,0 +1,30 @@
+// Objective
+// Implement an algorithm to determine if a string has all unique characters.  What if you cannot use additional data structures
+
+//Using hashmap
+function isUnique(str) {
+    var letterCount = {};
+    for (let i in str) {
+        if (letterCount[str[i]]) return false
+        else letterCount[str[i]] = str[i]
+    }
+    return true;
+}
+//Time Complexity : O(n^2)
+//Space Complexity : O(n)
+
+
+//Without other Data Structure
+function isUnique(str) {
+    while (str.length > 0) {
+        var curr = str.slice(-1);
+        str = str.slice(0, str.length - 1);
+        if (str.includes(curr)) return false
+    }
+    return true;
+}
+//Time Complexity : O(n) + .includes()
+//Space Complexity : O(1)
+
+
+isUnique("hlo");
