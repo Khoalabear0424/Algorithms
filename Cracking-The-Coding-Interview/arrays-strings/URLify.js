017924bc9e6963bdaae19d40f1512f0b15cdbbd9
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-04-28 14:11:09 -0700

    addd uglrify

---
+++ Cracking-The-Coding-Interview/arrays-strings/URLify.js
@@ -0,0 +1,9 @@
+isPermutation("dfffdg", "gdd");
+
+//-------------1.3------------//
+function URLify(str) {
+    var strArr = str.replace(/\s+/g, "%20");
+    console.log(strArr);
+}
+
+URLify("hello there     you");
