0c5e2c20913d40b23d54e117065449b69fc2d0cb
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-04-29 19:09:12 -0700

    finsihed bad example of is permutation of palindrome

--- Cracking-The-Coding-Interview/arrays-strings/is-permutations-of-palinfrome.js
+++ Cracking-The-Coding-Interview/arrays-strings/is-permutations-of-palinfrome.js
@@ -1,1 +1,20 @@
-code
+//-------------1.4------------//
+//Using includes
+function isPermutationOfPalindrome(str) {
+    str = str.replace(/\s+/g, "");
+    var counter = 0;
+    var letCount = {};
+    for (let i in str) {
+        letCount[str[i]] = (letCount[str[i]] || 1) * -1;
+    }
+    for (let i in letCount) {
+        if (letCount[i] < 0) {
+            counter++;
+            console.log(letCount[i])
+        }
+    }
+    console.log(counter)
+    return counter > 1 ? false : true;
+}
+
+isPermutationOfPalindrome('racedcarff')
