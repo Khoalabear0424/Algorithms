c199a08e79a29a10eb4f242cb1bb043dcdc2aa5b
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-04-29 20:49:10 -0700

    compress with constant time

--- Cracking-The-Coding-Interview/arrays-strings/1.6-compress-string.js
+++ Cracking-The-Coding-Interview/arrays-strings/1.6-compress-string.js
@@ -14,4 +14,19 @@ function compress(str) {
 //Time Complexity : O(2n) -> O(n)
 //Space Complexity : O(n) 
 
+//Constant Space
+function compress(str) {
+    var consecutiveLetter = 0;
+    var result = "";
+    for (var i in str) {
+        consecutiveLetter++;
+        if (str[i] != str[parseInt(i) + 1]) {
+            console.log(`str[i] = ${str[i]}, str[i+1] = ${str[i + 1]}`)
+            result += str[i] + consecutiveLetter;
+            consecutiveLetter = 0;
+        }
+    }
+    return result
+}
+
 compress('aaaabbbbrrrccc');
