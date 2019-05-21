68c7bae2eb6268ae2a54628ab5c1e29ad8fde98e
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-05-20 20:55:26 -0700

    added promise chainign

---
+++ CodeSignalAssessment/promiseChaning.js
@@ -0,0 +1,35 @@
+var promise1 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve(1), 1000);
+})
+
+promise1
+    .then((result) => {
+        console.log(result);
+        return result * 2
+    })
+    .then((result) => {
+        console.log(result)
+        return result * 2
+    })
+    .then((result) => {
+        console.log(result);
+        return result * 3
+    })
+
+var promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve(2), 2500);
+})
+
+var promise3 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve(3), 2000);
+})
+
+Promise.all([promise2, promise3])
+    .then((result) => {
+        console.log(result)
+        return result[0] * result[1]
+    })
+    .then((result) => {
+        console.log(result)
+    })
+
