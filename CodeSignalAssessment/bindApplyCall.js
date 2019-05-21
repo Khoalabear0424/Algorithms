0deda3fb6377a7948f306641d1f9a9204a004a99
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-05-20 20:25:08 -0700

    added example for call and apply

--- CodeSignalAssessment/bindApplyCall.js
+++ CodeSignalAssessment/bindApplyCall.js
@@ -8,9 +8,22 @@ var person = {
 
 person.getName()
 
+//-------------BIND EXAMPLE------------//
 var hobbie = function () {
     this.getName()
 }
 
 var hobbieBind = hobbie.bind(person)
 hobbieBind()
+
+//-------------Call Example-------------//
+//Call is similar to bind but will immediately call the function 
+//Takes additional parameters that must be declared individually
+
+var hobbie2 = function (hobby1, hobby2) {
+    console.log(hobby1, hobby2)
+    this.getName()
+}
+
+hobbie2.call(person, 'baskbetball', 'dancing')
+hobbie2.apply(person, ['baskbetball', 'dancing'])
